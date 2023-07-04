d5bc1e2972f3c01913e627c80789d8bf13b26324
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-04 17:07:11 -0500

    Create render_nested_components.jsx

---
+++ render_nested_components.jsx
@@ -0,0 +1,37 @@
+const TypesOfFruit = () => {
+  return (
+    <div>
+      <h2>Fruits:</h2>
+      <ul>
+        <li>Apples</li>
+        <li>Blueberries</li>
+        <li>Strawberries</li>
+        <li>Bananas</li>
+      </ul>
+    </div>
+  );
+};
+
+const Fruits = () => {
+  return (
+    <div>
+      <h1>These are fruits:</h1>
+      <TypesOfFruit />
+    </div>
+  );
+};
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        <Fruits />
+      </div>
+    );
+  }
+};
