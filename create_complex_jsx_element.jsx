ce48e2b2219fe409d51cf69813bc3e096a7eacc2
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-04 16:30:52 -0500

    Create create_complex_jsx_element.jsx

---
+++ create_complex_jsx_element.jsx
@@ -0,0 +1,11 @@
+const JSX = 
+
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is a paragraph.</p>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
