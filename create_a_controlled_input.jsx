da5e9f32666caf109c2bbbfc978652fc0babacc9
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-08 09:27:18 -0500

    Create create_a_controlled_input.jsx

---
+++ create_a_controlled_input.jsx
@@ -0,0 +1,26 @@
+class ControlledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    );
+  }
+}
