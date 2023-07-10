46a60b5ff4109fd34a18f93524cbda340b088486
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 20:36:24 -0500

    Create should_component_updates.jsx

---
+++ should_component_updates.jsx
@@ -0,0 +1,38 @@
+class OnlyEvens extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.value % 2 === 0;
+  }
+
+  componentDidUpdate() {
+    console.log('Component re-rendered.');
+  }
+
+  render() {
+    return <h1>{this.props.value}</h1>;
+  }
+}
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+    this.addValue = this.addValue.bind(this);
+  }
+
+  addValue() {
+    this.setState((state) => ({
+      value: state.value + 1
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value} />
+      </div>
+    );
+  }
+}
