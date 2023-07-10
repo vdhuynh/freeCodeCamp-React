e9b8a5911c42eeb643fe09d090067218dc873293
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 20:37:15 -0500

    Create inline_styles.jsx

---
+++ inline_styles.jsx
@@ -0,0 +1,12 @@
+class Colorful extends React.Component {
+  render() {
+    const colorfulStyle = {
+      color: 'red',
+      fontSize: '72px'
+    };
+
+    return (
+      <div style={colorfulStyle}>Big Red</div>
+    );
+  }
+}
